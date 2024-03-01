c58968f9c1d29ffc3472e1e59eb32046b44baa04
Mustafa Fahim <91342968+FahiimDeev@users.noreply.github.com>
2024-03-01 11:56:10 +0000

    all the datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strct" // all the codes will be recognised as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+console.log(3 
+    + 4) //code readability should be considered
+
+console.log("fahim")
+
+let name = ("fahim")
+let age = 23
+let isLoggedIn = true
+
+//number => bigint which range is 2 to the power 53
+//string => ""
+//boolean => true/false
+//null => standalone value / representation of empty value
+// undefined => 
+// symbol => unique
+
+//object => 
+
+console.log(typeof undefined); // output will be undefined
+console.log(typeof null); // output will be object
+
+
