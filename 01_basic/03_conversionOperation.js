64dbb564a3abcfbdf212a6ce81d291e5e38e12aa
Mustafa Fahim <91342968+FahiimDeev@users.noreply.github.com>
2024-03-01 13:17:17 +0000

    Conversion of Datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "fahim"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+// "45" => 45
+// "45fdsf" => NaN ( Not a Number )
+// true => 1 ; false => 0
+
+let isLoggedIn = "fahim"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "fahim" => true
+
+let number = 45
+
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
