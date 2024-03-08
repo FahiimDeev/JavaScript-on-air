aabd90584b06492832f6f8c2e924f14914fed5f8
Mustafa Fahim <91342968+FahiimDeev@users.noreply.github.com>
2024-03-08 16:03:49 +0000

    functions deep

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,4 +33,45 @@ function logInUserMessage(username = "Mustafa"){
     return `${username} just slide in!`
 }
 
-console.log(logInUserMessage("fahim"));
+// console.log(logInUserMessage("fahim"));
+
+function calculateCartPrice(val1, val2, ...number1){
+    return number1
+}
+
+console.log(calculateCartPrice(590, 659, 450, 34534,546));
+
+const user = {
+    username: "Mustafa",
+    price: 190 
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "Fahim",
+    price:500
+})
+
+const myNewArray = [100, 200, 300, 400]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue([999,888,777,666]));
+
+//another self practice
+
+const greatMeat = ["Chicken", "Mutton", "Beef"]
+
+function whichTastesBetter(meat){
+    return meat[2]
+}
+
+// console.log(whichTastesBetter(greatMeat));
+console.log(whichTastesBetter(["burger", "nachos", "pizza"]));
